e865f9d709752954855ec9e81aff335bba5d003d
LongPB <longpb.91@gmail.com>
2022-12-27 13:41:38 +0700

    update run_kubernetes and upload_docker

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=longpb/ml-microservice-kubernetes:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy python-helloworld --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get nodes
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward longpb/ml-microservice-kubernetes 8080:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=longpb/ml-microservice-kubernetes
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag ml-microservice-kubernetes:latest $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
