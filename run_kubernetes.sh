eee86eb2a26d5e4c1875f8a1ae623993fa7335db
LongPB <longpb.91@gmail.com>
2022-12-27 14:24:38 +0700

    edit run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,16 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-dockerpath=longpb/ml-microservice-kubernetes:latest
+dockerpath="longpb/ml-microservice-kubernetes:latest"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl create deploy python-helloworld --image=$dockerpath
+kubectl run ml-k8s-predict --image="$dockerpath" --port=80
 
 # Step 3:
 # List kubernetes pods
-kubectl get nodes
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward longpb/ml-microservice-kubernetes 8080:80
+kubectl port-forward ml-k8s-predict 8080:80
